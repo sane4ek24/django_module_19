6cf57d7e449d2e6b5b3ab6fcb3a344f453d60541
sane4ek24 <119444641+sane4ek24@users.noreply.github.com>
2024-12-14 19:55:44 +0500

    module_19_4.py commit

--- django1/task1/admin.py
+++ django1/task1/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Buyer, Game
 
-# Register your models here.
+
+@admin.register(Buyer)
+class Buyer(admin.ModelAdmin):
+    list_display = ('name', 'balance', 'age')
+    list_filter = ('balance', 'age')
+    search_fields = ('name',)
+    list_per_page = 30
+    readonly_fields = ('balance',)
+
+
+@admin.register(Game)
+class Game(admin.ModelAdmin):
+    list_display = ('title', 'cost', 'size')
+    list_filter = ('size', 'cost')
+    search_fields = ('title',)
+    list_per_page = 20
